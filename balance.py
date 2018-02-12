0d5d9a1aa1f397f7b22e735e8465c5d33e13b580
Lee Aspitarte <aspitarte@gmail.com>
2018-02-11 21:12:42 -0800

    add ability to retry with requests and also add printing balances to command line and clearing

--- balance.py
+++ balance.py
@@ -9,6 +9,8 @@ import time
 import csv
 import datetime
 import os
+import numpy as np
+import sys
 
 filename = "defaults.txt"
 f = open(filename, 'r')
@@ -28,114 +30,135 @@ parser.add_argument('-o', metavar='output', default=Output, choices=["text", "cs
 args = parser.parse_args()
 
 symbols = {
-	  "adzcoin": "ADZ",
-	  "auroracoin": "AUR",
-	  "bitcoin": "BTC",
-	  "bitcoin-cash": "BCH",
-	  "bitcoin-gold": "BTG",
-	  "dash": "DSH",
-	  "digibyte": "DGB",
-	  "digibyte-groestl": "DGB",
-	  "digibyte-skein": "DGB",
-	  "digibyte-qubit": "DGB",
-	  "ethereum": "ETH",
-	  "ethereum-classic": "ETC",
-	  "expanse": "EXP",
-	  "feathercoin": "FTC",
-	  "gamecredits": "GAME",
-	  "geocoin": "GEO",
-	  "globalboosty": "BSTY",
-	  "groestlcoin": "GRS",
-	  "litecoin": "LTC",
-	  "maxcoin": "MAX",
-	  "monacoin": "MONA",
-	  "monero": "XMR",
-	  "musicoin": "MUSIC",
-	  "myriadcoin": "XMY",
-	  "myriadcoin-skein": "XMY",
-	  "myriadcoin-groestl": "XMY",
-	  "myriadcoin-yescrypt": "XMY",
-	  "sexcoin": "SXC",
-	  "siacoin": "SC",
-	  "startcoin": "START",
-	  "verge": "XVG",
-	  "vertcoin": "VTC",
-	  "zcash": "ZEC",
-	  "zclassic": "ZCL",
-	  "zcoin": "XZC",
-	  "zencash": "ZEN"
+      "adzcoin": "ADZ",
+      "auroracoin": "AUR",
+      "bitcoin": "BTC",
+      "bitcoin-cash": "BCH",
+      "bitcoin-gold": "BTG",
+      "dash": "DSH",
+      "digibyte": "DGB",
+      "digibyte-groestl": "DGB",
+      "digibyte-skein": "DGB",
+      "digibyte-qubit": "DGB",
+      "ethereum": "ETH",
+      "ethereum-classic": "ETC",
+      "expanse": "EXP",
+      "feathercoin": "FTC",
+      "gamecredits": "GAME",
+      "geocoin": "GEO",
+      "globalboosty": "BSTY",
+      "groestlcoin": "GRS",
+      "litecoin": "LTC",
+      "maxcoin": "MAX",
+      "monacoin": "MONA",
+      "monero": "XMR",
+      "musicoin": "MUSIC",
+      "myriadcoin": "XMY",
+      "myriadcoin-skein": "XMY",
+      "myriadcoin-groestl": "XMY",
+      "myriadcoin-yescrypt": "XMY",
+      "sexcoin": "SXC",
+      "siacoin": "SC",
+      "startcoin": "START",
+      "verge": "XVG",
+      "vertcoin": "VTC",
+      "zcash": "ZEC",
+      "zclassic": "ZCL",
+      "zcoin": "XZC",
+      "zencash": "ZEN"
 }
 
 def get_value(symbol, amount, compare=args.c):
-	"""
-	Convert some amount of some coin, into the currency specified by -c
-	"""
-	if symbol.upper() == compare.upper():
-		return amount
-	url = "https://api.cryptonator.com/api/ticker/{}-{}".format(symbol.lower(), compare.lower())
-	raw_response = requests.get(url).text
-	response = json.loads(raw_response)
-	if(response["success"]):
-		price = response["ticker"]["price"]
-		value = float(price) * float(amount)
-		return value
-	else:
-		return 0 #returns zero if the coin is not on the ticker website
-
+    """
+    Convert some amount of some coin, into the currency specified by -c
+    """
+    if symbol.upper() == compare.upper():
+        return amount
+    
+    num_attempts = 10
+    for attempt in range(num_attempts):
+        url = "https://api.cryptonator.com/api/ticker/{}-{}".format(symbol.lower(), compare.lower())
+        raw_response = requests.get(url).text    
+        print("ticker " +symbol + " attempt #" + str(attempt + 1))
+        try:
+            response = json.loads(raw_response)
+            if(response["success"]):
+                price = response["ticker"]["price"]
+                value = float(price) * float(amount)
+                return value
+            else:
+                return 0 #returns zero if the coin is not on the ticker website
+        except json.decoder.JSONDecodeError:
+            print("JSON decode error from ticker " + symbol + ", trying again...")
+            time.sleep(1)
+            
+    sys.exit("couldn't get response from website")
+    return 0
 #log_filename  = datetime.date.today().strftime('Data\monitorlog_%Y-%m-%d.csv')
 
 def obtain_mph_balance():
-	log_filename  = ('Data\monitorlog.csv')
-	should_write_header = 1
-	should_write_header = int(not (os.path.exists(log_filename)))
-	# Query the MPH API to get all current balances
-	
-	url = "https://miningpoolhub.com/index.php?page=api&action=getuserallbalances&api_key={}".format(args.a)
-	raw_response = requests.get(url).text
-	response = json.loads(raw_response)
-	coindata = []
-	coinheaders = []
+    os.system('clear')
+    log_filename  = ('Data\monitorlog.csv')
+    should_write_header = 1
+    should_write_header = int(not (os.path.exists(log_filename)))
+    # Query the MPH API to get all current balances
+    
+    url = "https://miningpoolhub.com/index.php?page=api&action=getuserallbalances&api_key={}".format(args.a)
+    raw_response = requests.get(url).text
+    response = json.loads(raw_response)
+    coindata = []
+    coinheaders = []
 
-		# Parse the response into a basic dictionary keyed on coin name
-	coins = {}
-	for coin in response["getuserallbalances"]["data"]:
-		symbol = symbols[coin["coin"]]
-		balance = sum([
-			coin["confirmed"],
-			coin["unconfirmed"],
-			coin["ae_confirmed"],
-			coin["ae_unconfirmed"],
-			coin["exchange"]
-			   ])
-		coins[symbol] = balance
-		if(balance != 0):
-			coinheaders.extend([symbol])
-			coindata.extend([balance])
-
-
-		  # Get the total value of all the coin balances
-	value = sum([get_value(coin, coins[coin], args.c) for coin in coins])
-	fiat_value = get_value(args.c, value, args.f)
-
-	  # Print report
-
-	coindata = [int(time.time()),value,round(fiat_value, 3)] + coindata
-	coinheaders = ["Time","BTC_total_value","Fiat_total_value"] + coinheaders
-
-	if should_write_header:
-		with open(log_filename, 'w') as f:
-			Writer = csv.writer(f, lineterminator='\n')
-			print(coinheaders)
-			Writer.writerow(coinheaders)
-			should_write_header = 0
+        # Parse the response into a basic dictionary keyed on coin name
+    coins = {}
+    for coin in response["getuserallbalances"]["data"]:
+        symbol = symbols[coin["coin"]]
+        balance = sum([
+            coin["confirmed"],
+            coin["unconfirmed"],
+            coin["ae_confirmed"],
+            coin["ae_unconfirmed"],
+            coin["exchange"]
+               ])
+        coins[symbol] = balance
+        if(balance != 0):
+            coinheaders.extend([symbol])
+            coindata.extend([balance])
 
 
+          # Get the total value of all the coin balances
+    valuelist = {}
+    value = 0
+    for coin in coins:
+        valuelist[coin] = get_value(coin, coins[coin], "btc")
+        value = value + valuelist[coin]
+    fiat_value = get_value("btc", value, "usd")
+    
+    
+    os.system('clear')
+    for coin in valuelist:
+        print(coin +": " + str(coins[coin]) + " worth " + str(valuelist[coin]) + " BTC ")
+    print("Total Value (BTC): " + str(value))
+    print("Total Value (USD): " + str(fiat_value))
+      # Print report
 
-	with open(log_filename, 'a') as f:
-		Writer = csv.writer(f, lineterminator='\n')
-		Writer.writerow(coindata)
-	#print(coindata)
-	time.sleep(50)
+    coindata = [int(time.time()),value,round(fiat_value, 3)] + coindata
+    coinheaders = ["Time","BTC_total_value","Fiat_total_value"] + coinheaders
+
+    if should_write_header:
+        with open(log_filename, 'w') as f:
+            Writer = csv.writer(f, lineterminator='\n')
+            print(coinheaders)
+            Writer.writerow(coinheaders)
+            should_write_header = 0
+
+
+
+    with open(log_filename, 'a') as f:
+        Writer = csv.writer(f, lineterminator='\n')
+        Writer.writerow(coindata)
+    #print(coindata)
+    time.sleep(50)
 
 #if __name__ == "__main__":
-#	main()
+#    main()
